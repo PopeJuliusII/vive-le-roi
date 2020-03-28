b7622e1e519ad4de5066c00baf96f9ea2924bbc5
PopeJuliusII <edgar@Pactolus.local>
2020-03-28 12:03:44 -0400

    "styled menu items"

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -45,8 +45,8 @@ class Directory extends React.Component {
     render() {
         return(
         <div className='directory-menu'>
-            {this.state.sections.map(({title, imageUrl, id}) => (
-                <MenuItem key={id} title={title} />
+            {this.state.sections.map(({title, imageUrl:Url, id, size:s}) => (
+                <MenuItem key={id} title={title} imageUrl={Url} size={s}/>
             ))}
         </div>
     );
