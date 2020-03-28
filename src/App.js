ca3e8ceb536ce8f7b863a70e2f0086708785a970
PopeJuliusII <edgar@Pactolus.local>
2020-03-28 16:36:55 -0400

    test hats page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+    <div>
+        <h1>HAAAAATS</h1>
+    </div>
+);
+
 function App() {
   return (
     <div>
-        <HomePage />
+        <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/hats' component={HatsPage} />
+        </Switch>
     </div>
   );
 }
