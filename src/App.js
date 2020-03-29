e22d115e752f4e3fbb67292ecc0af3911b8cb05f
PopeJuliusII <edgar@Pactolus.local>
2020-03-29 14:04:37 -0400

    google auth next step

--- src/App.js
+++ src/App.js
@@ -7,10 +7,33 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
+class App extends React.Component{
+    constructor(){
+        super();
 
-function App() {
-  return (
+        this.state = {
+            currentUser: null
+        }
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentDidMount() {
+        auth.onAuthStateChanged(user =>{
+            this.setState({ currentUser: user });
+
+            console.log(user)
+        })
+    }
+
+    componentWillUnmount(){
+        this.unsubscribeFromAuth();
+    }
+
+  render() {
+      return (
     <div>
         <Header />
         <Switch>
@@ -19,7 +42,7 @@ function App() {
             <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
     </div>
-  );
+);}
 }
 
 export default App;
